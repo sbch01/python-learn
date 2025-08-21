8eea94081eef962edb40dd9d2fe3ca3efc55d11d
sbch01 <banchev2001@abv.bg>
2025-08-21 22:07:11 +0300

    calculation lesson new lines

---
+++ 1-lesson-math-operations.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+#Here is first lesson a brief demonstration how python calculate math equation
+
+#Add two numbers
+print("Add 2+2 =", 2+2)
+
+#Prority of operation examlpe
+print("Equation 10 + 2*3 =", 10+2*3)
+
+#Divasion, the result is awlays float number
+print("Equation 22/7 =",22/7)
+
+#Here is divasion that discard the fraction
+print("Discard fraction divide 22//7 =",22//7)
+
+#Here is power calculation in python **
+print("This is 2 power of 8: 2**8 =", 2**8)
+
+#The reminder of divasion
+print("This give only reminder of division 17%5 =", 17%5)
+
+#Assign a variable
+long = 10
+width = 20
+high = 20
+
+volume = long * high * width
+
+print("The volume", volume)
