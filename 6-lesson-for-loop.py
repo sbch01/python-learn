65508d30d8b236811862bb5c05c8cc704205caeb
sbch01 <banchev2001@abv.bg>
2025-09-18 14:16:36 +0300

    for lesson add

--- 6-lesson-for-loop.py
+++ 6-lesson-for-loop.py
@@ -11,3 +11,17 @@ words = ['cat', 'window', 'defenestrate']
 for w in words:
 
 	print(w, len(w))
+
+#loop all list  and if length of some member is larger then 6 insert once more in the beginig of the list
+#===============================================
+for w in words[:]:
+	if len(w) > 6:
+		words.insert(0, w)
+
+print(words)
+
+#Range example in for loop
+#===============================================
+
+for i in range(1,5):
+	print (i)
