5f5fb9fa4682c3bc415e02d864c252fa35d99335
sbch01 <banchev2001@abv.bg>
2025-09-30 21:04:41 +0300

    new continue break lessons

--- 6-lesson-for-loop.py
+++ 6-lesson-for-loop.py
@@ -16,6 +16,7 @@ print()
 #loop all list  and if length of some member is larger then 6 insert once more in the beginig of the list
 #===============================================
 for w in words[:]:
+
 	if len(w) > 6:
 		words.insert(0, w)
 
@@ -26,20 +27,33 @@ print("\n")
 #===============================================
 
 for i in range(1,5):
+
 	print (i, end=" ")
 print("\n")
 
 #Here is little bit diffrent
 for i in range(5):
+
 	print(i, end=" ")
 print("\n")
 
 #Here is example that loop in range 2 to 10 with step 2
 for i in range(2,10,2): #(from, to, step)
+
 	print(i, end=" ")
 print("\n")
 
 #Once more with negative numbers
 for i in range(-2,-10,-2): #(from, to, step)
+
 	print(i, end=" ")
 print("\n")
+
+#Once more combination with range and len
+#================================================
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+
+for i in range(len(a)):
+
+	print (i, a[i])
+
---
+++ 7-lesson-break-and-continue.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+for n in range(2, 10):
+	for x in range(2, n):
+		if n % x == 0:
+			print(n, 'equals', x, '*', n//x)
+			break #when reach break else clause of for loop is not executed
+	else:#here is a strange in python that else is part of for loop not part of if statement
+		# loop fell through without finding a factor
+		print(n, 'is a prime number')
+
+#There is more examples for exeptions in future of this tutorial
+
+for i in range (2,10):
+	if i % 2 == 0:
+		print ('found even number', i)
+		continue
+	print ('found a number', i)
