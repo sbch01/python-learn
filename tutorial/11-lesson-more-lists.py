2d2ed4fa7a36948d5ab99807509a18c0669974d9
sbch01 <banchev2001@abv.bg>
2025-11-06 17:08:04 +0200

    extended list example

---
+++ tutorial/11-lesson-more-lists.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+#Here is more about lists as a object 
+#We gone to play with some methods of list object
+
+#crete two list of names and animals
+newlist = ["john","stoian","peter","stoian"]
+iterable = ['dog','cat','mouse']
+
+#count item in list
+#--------------------------------------------
+print(newlist.count('stoian'))
+
+#append on list
+#--------------------------------------------
+newlist.append("atanas") #add item on the tail of list
+print(newlist)
+
+#extend on list
+#--------------------------------------------
+newlist.extend(iterable) #adding list iterable to the first one newlist
+print(newlist)
+
+#isert on list
+#--------------------------------------------
+newlist.insert(1, 'newname') #add newname member in list after first member in the list
+print(newlist)
+
+#remove from list
+#--------------------------------------------
+newlist.remove('stoian') #search stoian in list and remove the first item with that name
+print(newlist)
+
+#pop from list
+#--------------------------------------------
+lastname = newlist.pop() #remove last item from list and put it to lastname var
+firstname = newlist.pop(0) #remove first item from list and put it to firstname var
+#print the result
+print(lastname)
+print(firstname)
+print(newlist)
+
+#sort list
+#--------------------------------------------
+newlist.sort() #here is sorting list in alphabet sequence
+print(newlist)
+
+#reverse list
+#--------------------------------------------
+newlist.reverse()
+print(newlist)
+
+#index return list
+#--------------------------------------------
+print(newlist.index('stoian')) #return index number of stoian item in list
