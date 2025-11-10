d711898b3abac203128c5a0c31d12ab4d5eeda39
sbch01 <banchev2001@abv.bg>
2025-11-10 22:08:56 +0200

    few more lists

--- tutorial/12-lesson-lists-comprehensions.py
+++ tutorial/12-lesson-lists-comprehensions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 #Here is more about lists as a object 
-#list comprehension is about aotmation creat list of some series in a ratianoal way
+#list comprehension is about automation creat list of some series in a ratianoal way
 
 #this creat list of numbers  square of two 
 #--------------------------------------------
@@ -25,3 +25,22 @@ vec = [-3, -1, 1, 4, 6]
 print(vec)
 print([x*2 for x in vec]) #here is double the numbers in vec
 print([abs(x) for x in vec]) #here make all numbers positive
+print([(x, x**2) for x in range(6)]) #create list of tuples
+
+#let creat the matrix
+matrix = [[1,2,3,4],
+          [5,6,7,8],
+          [9,10,11,12]]
+#and now lets transpose this matrix
+trans = [[row[i] for row in matrix] for i in range(4)] 
+print(trans)
+
+#del statment examples
+#--------------------------------------------
+a = [-1, 1, 66.25, 333, 333, 1234.5]
+del a[0]
+print(a)
+del a[2:4]
+print(a)
+del a[:] #also can be used del a without [:]
+print(a)
